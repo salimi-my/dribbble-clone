4964d55ca5699039648128da39accc809c0e8646
Mohamad Salimi <contact@salimi.my>
2023-12-06 00:27:29 +0800

    Update sign in page layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,14 +8,16 @@ export default function AuthLayout({
 }) {
   return (
     <div className='flex items-stretch h-screen overflow-hidden'>
-      <section className='w-[400px] grow-0 relative text-white'>
+      <section className='hidden lg:flex w-[400px] grow-0 relative text-white'>
         <div className='flex flex-col justify-between h-full'>
-          <Link href='/' className='absolute top-10 left-10'>
+          <Link href='/' className='absolute top-10 left-10 z-10'>
             <Image
-              src='/bribbble-logo.svg'
+              src='/bribbble-light-logo.svg'
               alt='bribbble logo'
+              className='w-[90px] h-[30px]'
               width={90}
-              height={31}
+              height={30}
+              priority
             />
           </Link>
           <video
@@ -26,10 +28,15 @@ export default function AuthLayout({
             muted
             src='sign-in.mp4'
           />
+          <Link href='/' className='absolute bottom-10 left-10 text-sm'>
+            @glebich
+          </Link>
         </div>
       </section>
       <section className='flex flex-col flex-1 overflow-auto justify-center w-full'>
-        {children}
+        <main className='flex justify-center xl:justify-start grow items-center w-full h-full'>
+          <div className='xl:ml-28'>{children}</div>
+        </main>
       </section>
     </div>
   );
