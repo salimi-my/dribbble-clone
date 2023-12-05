0d332d425d3ddace8fa1852caea437e530229f24
Mohamad Salimi <contact@salimi.my>
2023-12-06 00:42:43 +0800

    Update sign up page layout

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,42 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import { SignIn } from '@clerk/nextjs';
 
 export default function SignInPage() {
-  return <SignIn />;
+  return (
+    <>
+      <section className='hidden lg:flex w-[400px] grow-0 relative text-white'>
+        <div className='flex flex-col justify-between h-full'>
+          <Link href='/' className='absolute top-10 left-10 z-10'>
+            <Image
+              src='/bribbble-light-logo.svg'
+              alt='bribbble logo'
+              className='w-[90px] h-[30px]'
+              width={90}
+              height={30}
+              priority
+            />
+          </Link>
+          <video
+            playsInline
+            className='w-full object-cover h-full'
+            autoPlay
+            loop
+            muted
+            src='sign-in.mp4'
+          />
+          <Link href='/' className='absolute bottom-10 left-10 text-sm'>
+            @glebich
+          </Link>
+        </div>
+      </section>
+      <section className='flex flex-col flex-1 overflow-auto justify-center w-full'>
+        <main className='flex justify-center xl:justify-start grow items-center w-full h-full'>
+          <div className='xl:ml-28'>
+            <SignIn />
+          </div>
+        </main>
+      </section>
+    </>
+  );
 }
