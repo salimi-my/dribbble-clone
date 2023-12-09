f807695c2ba5ee67d79ae26a0e576abc980d79bd
Mohamad Salimi <contact@salimi.my>
2023-12-09 22:38:42 +0800

    Update auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,6 @@ export default function AuthLayout({
   children
 }: {
   children: React.ReactNode;
-  params: string;
 }) {
   return (
     <div className='flex items-stretch h-screen overflow-hidden'>
