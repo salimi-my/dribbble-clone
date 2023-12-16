7a7afead60f020f705ce4ca6389e4c35d3958a48
Mohamad Salimi <contact@salimi.my>
2023-12-16 10:01:32 +0800

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,13 @@ import { authMiddleware } from '@clerk/nextjs';
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ['/', '/sign-in', '/sign-up', '/api/project']
+  publicRoutes: [
+    '/',
+    '/sign-in',
+    '/sign-up',
+    '/api/project',
+    '/api/user/:path*'
+  ]
 });
 
 export const config = {
